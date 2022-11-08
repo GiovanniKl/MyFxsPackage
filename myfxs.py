0dc25181f4a19d388ee42086c3bc5a7bac739f52
Jan Klíma <johnnyklima@gmail.com>
2022-11-08 10:30:00 +0100

    Typographical advancements.
    Changed # ... and '''...''' comments in description of functions to """...""" (double quoted block comments), plus some minor imporvements (redundant/absent sapaces, parentheses, ...).

--- myfxs.py
+++ myfxs.py
@@ -8,27 +8,27 @@ Sorry for Czech comments. Sometimes they seem apropriate.
 There is no guarantee of compatibility with scripts using this module written
 before last update.
 Created by Jan Klíma on 2022-03-04.
-Updated on 2022-07-05.
+Updated on 2022-11-08.
 """
 
 
 def getr2(y, yfit):
-    # returns sum of residual squares R^2
-    # y - numpy array of experimental data
-    # yfit - numpy array of fit data
+    """Returns sum of residual squares R^2.
+    y - numpy array of experimental data
+    yfit - numpy array of fit data"""
     ss_res = np.sum((y - yfit)**2)
     ss_tot = np.sum((y - np.mean(y))**2)
     return 1 - ss_res/ss_tot
 
 
 def zaoknem(x):
-    # == ZAOKrouhliNEjistotu(M) (round uncertainty)
-    # returns rounded uncertainty x and an index ind, where to round the main
-    # value, e.g. using np.round(x0, ind)
-    # Use only positive numbers!
-    # In previous versions, there was in the first if number 1, which was later
-    # changed to 3 and short after that replaced with variable koef = 2.95. Some
-    # aditional changes had to be made (e.g. int()s inside str()s).
+    """== ZAOKrouhliNEjistotu(M) (round uncertainty)
+    Returns rounded uncertainty x and an index ind, where to round the main
+    value, e.g. using np.round(x0, ind).
+    Use only positive numbers!
+    In previous versions, there was in the first if number 1, which was later
+    changed to 3 and short after that replaced with variable koef = 2.95. Some
+    additional changes had to be made (e.g. int()s inside str()s)."""
     koef = 2.95
     if x > koef:
         do, i = True, koef*10**1
@@ -57,7 +57,8 @@ def zaoknem(x):
 
 
 def makemyfontnice():
-    # sets all font types used in plots to look cool (i.e. serif roman/italic)
+    """Sets all font types used in plots to look cool (i.e. serif roman/italic).
+    """
     rcParams["font.family"] = "serif"
     rcParams["mathtext.it"] = "serif:italic"
     rcParams["mathtext.cal"] = "serif:italic"
@@ -67,26 +68,26 @@ def makemyfontnice():
 
 
 def linab(x, a, b):
-    # linear function y = a*b + b
+    """Linear function y = a*b + b."""
     return a*x + b
 
 
 def lina(x, a):
-    # linear function intersecting at zero y = a*x
+    """Linear function intersecting at zero y = a*x."""
     return a*x
 
 
 def vypoctinejistotu(xs, nejb=np.zeros(1), quantity="X", units="x", otype="g",
                      out=True):
-    # Calculates standard deviation and mean using 95% confidence interval of
-    # Student's distribution.
-    # xs - array-like of experimental data
-    # nejb - array-like (optional) of type B uncertainties
-    # quantity - string (optional) containing quantity name
-    # units - string (optional) containing units name
-    # otype - string (optional), valid format_spec after ":", e.g. "03.2f"
-    # out - bool (optional), whether to print results in the terminal
-    # returns xbar (mean value of x) and nej (deviation) as floats
+    """Calculates standard deviation and mean using 95% confidence interval of
+    Student's distribution.
+    xs - array-like of experimental data
+    nejb - array-like (optional) of type B uncertainties
+    quantity - string (optional) containing quantity name
+    units - string (optional) containing units name
+    otype - string (optional), valid format_spec after ":", e.g. "03.2f"
+    out - bool (optional), whether to print results in the terminal
+    returns xbar (mean value of x) and nej (deviation) as floats"""
     def koef(n):
         # vraci koeficient 'k' dle t-rozdeleni pro p=95%
         koefsl = [12.71, 4.3, 3.18, 2.78, 2.57, 2.45, 2.36, 2.31, 2.26, 2.23,
@@ -94,24 +95,24 @@ def vypoctinejistotu(xs, nejb=np.zeros(1), quantity="X", units="x", otype="g",
         koefsh = {25: 2.06, 30: 2.04, 35: 2.03, 40: 2.02, 45: 2.01, 50: 2.01,
                   100: 1.984, 101: 1.96}
         if n < 21:
-            k = koefsl[n-1]
+            ko = koefsl[n - 1]
         elif n < 26:
-            k = koefsh[25]
+            ko = koefsh[25]
         elif n < 31:
-            k = koefsh[30]
+            ko = koefsh[30]
         elif n < 36:
-            k = koefsh[35]
+            ko = koefsh[35]
         elif n < 41:
-            k = koefsh[40]
+            ko = koefsh[40]
         elif n < 46:
-            k = koefsh[45]
+            ko = koefsh[45]
         elif n < 51:
-            k = koefsh[50]
+            ko = koefsh[50]
         elif n < 101:
-            k = koefsh[100]
+            ko = koefsh[100]
         else:
-            k = koefsh[101]
-        return k
+            ko = koefsh[101]
+        return ko
     nmer = len(xs)
     k = koef(nmer)
     xbar = sum(xs)/nmer
@@ -127,7 +128,7 @@ def vypoctinejistotu(xs, nejb=np.zeros(1), quantity="X", units="x", otype="g",
     nej, par = zaoknem(nej)
     xbar = round(xbar, par)
     if nej == int(nej):
-       xbar, nej = int(xbar), int(nej)
+        xbar, nej = int(xbar), int(nej)
     if out:
         outstr = "{}: ({:" + otype + "} +- {:" + otype + "}) {}"
         print(outstr.format(quantity, xbar, nej, units))
@@ -135,15 +136,15 @@ def vypoctinejistotu(xs, nejb=np.zeros(1), quantity="X", units="x", otype="g",
     return xbar, nej
 
 
-def prin(t, d = 50, o = 2, ws=True):
-    # Function to print desired text into the shell, centered and filled with
-    #   asteriscs (*) to the length d, padded with space before and after the
-    #   text if ws=True.
-    # t - string, text to print for best appearance shorter than d
-    # d - int (optional), length of the full row
-    # o - int (optional), minimal offset/padding at the beggining when k > d-o
-    # ws - bool (optional), padding the text with one whitespace on each side
-    # (if len(t)>d-o*2: len(row)=len(t)+o)
+def prin(t, d=50, o=2, ws=True):
+    """Function to print desired text into the shell, centered and filled with
+    asteriscs (*) to the length d, padded with space before and after the
+    text if ws=True.
+    t - string, text to print for best appearance shorter than d
+    d - int (optional), length of the full row
+    o - int (optional), minimal offset/padding at the beggining when k > d-o
+    ws - bool (optional), padding the text with one whitespace on each side
+        (if len(t)>d-o*2: len(row)=len(t)+o)"""
     k = len(t)
     if ws:
         if k < d-o*2-2:
@@ -154,9 +155,8 @@ def prin(t, d = 50, o = 2, ws=True):
             print("*"*o+" "+t)
     else:
         if k < d-o*2:
-            print("*"*((d-k)//2+(d-k) % 2)+t+" "+"*"*(d-k)//2)
+            print("*"*((d-k)//2+(d-k) % 2)+t+" "+"*"*((d-k)//2))
         elif k < d-o:
             print("*"*o+t+"*"*(d-k-o))
         else:
             print("*"*o+t)
-
--- myimages.py
+++ myimages.py
@@ -4,8 +4,8 @@ from PIL import Image
 
 
 def imresiz():
-    '''This script resizes pics to whatever (reasonable, e.g. not 0) scale you
-    want in interactive, console-text-input way.'''
+    """This script resizes pics to whatever (reasonable, e.g. not 0) scale you
+    want in interactive, console-text-input way."""
     mfs.prin("Welcome to Image Resizer!")
     exts = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".mpeg"]
     do = True
@@ -13,7 +13,7 @@ def imresiz():
         do0 = True
         while do0:
             try:
-                pat = input("Gimme a path to image folder (empty for "+
+                pat = input("Gimme a path to image folder (empty for " +
                             "current folder): ")
                 if pat == "":
                     pat = os.getcwd()
@@ -27,7 +27,7 @@ def imresiz():
                             count += 1
                             ims.append(i)
                 if count == 0:
-                    print("No images found, try different folder or add "+
+                    print("No images found, try different folder or add " +
                           "extension to list in the script.")
                     raise OSError
                 do0 = False
@@ -35,8 +35,8 @@ def imresiz():
                 print("Wrong path format, please try again.")
         do0 = True
         while do0:
-            resize_inds = input("Enter index (or list of indices in []) of"+
-                                " pictures you want to resize "+
+            resize_inds = input("Enter index (or list of indices in []) of" +
+                                " pictures you want to resize " +
                                 "(empty for all): ")
             try:
                 if resize_inds == "":
@@ -54,15 +54,15 @@ def imresiz():
                     raise SyntaxError
                 if any([i > count for i in resiz_inds]):
                     do0 = True
-                    print("Some indices are larger than expected! Choose "+
+                    print("Some indices are larger than expected! Choose " +
                           "only from displayed values.")
                     raise SyntaxError
             except (SyntaxError, ValueError, NameError):
-                print("Wrong format, please enter integers or list of "+
+                print("Wrong format, please enter integers or list of " +
                       "integers.")
         do0, scalall = True, False
         while do0:
-            scale = input("Enter new scaling parameter for all images or each"+
+            scale = input("Enter new scaling parameter for all images or each" +
                           " image (as a list): ")
             try:
                 if type(eval(scale)) == list:
@@ -71,17 +71,17 @@ def imresiz():
                         do0 = False
                 elif type(eval("["+scale+"]")) == list:
                     scal = eval("["+scale+"]")
-                    if all([isinstance(i, (float,int)) for i in scal]):
+                    if all([isinstance(i, (float, int)) for i in scal]):
                         do0 = False
                     if len(scal) == 1:
                         scalall = True
                 if len(resiz_inds) != len(scal) and len(scal) != 1:
-                    print("Wrong size of scaling parameter list (must be "+
+                    print("Wrong size of scaling parameter list (must be " +
                           "same size list as list of image indices or float).")
                     do0 = True
                     raise SyntaxError
             except (SyntaxError, ValueError, NameError):
-                print("Wrong format, please enter integers or list of "+
+                print("Wrong format, please enter integers or list of " +
                       "integers.")
         do0 = True
         while do0:
@@ -101,10 +101,10 @@ def imresiz():
                 j = 0
             else:
                 j = i
-            im = Image.open(os.path.join(pat,ims[i]))
+            im = Image.open(os.path.join(pat, ims[i]))
             im = im.resize((round(im.width*scal[j]), round(im.height*scal[j])))
-            im.save(os.path.join(pats, ims[i][:ims[i].rfind(".")]+
-                                 "_sc{}".format(scal[j]).replace(".", "p")+
+            im.save(os.path.join(pats, ims[i][:ims[i].rfind(".")] +
+                                 "_sc{}".format(scal[j]).replace(".", "p") +
                                  ims[i][ims[i].rfind("."):]))
             im.close()
         mfs.prin("Finished.")
@@ -124,25 +124,25 @@ def imresiz():
 
 def imjoin(impaths, spath, poses, padding=0, box=None, bg="#ffffff00",
            align="center center"):
-    '''Joins given images into a predefined rectangular grid and saves the
+    """Joins given images into a predefined rectangular grid and saves the
     final image. Favourable format is PNG, compatibility with other images
     is not guaranteed. This may change in future releases.
     impaths - list of paths (strings) to every image
     spath - path string to save the final image
-    poses - 2-d array (or list of lists) with positions of each image, 
+    poses - 2-d array (or list of lists) with positions of each image,
     use 0 where no image will be located and index images from 1
     padding - int, number of pixels of padding around each image
     box - 2-tuple of int values of size of each position, default are largest
     dimensions of all images
     bg - background color hex string in "#rrggbbaa" format
-    align - location of image in its box, same as in matplotlib legend loc'''
+    align - location of image in its box, same as in matplotlib legend loc"""
     ims, size = [Image.open(i) for i in impaths], [0, 0]
     for i in range(len(impaths)):
         if ims[i].size[0] > size[0]:
             size[0] = ims[i].size[0]
         if ims[i].size[1] > size[1]:
             size[1] = ims[i].size[1]
-    if box != None:
+    if box is not None:
         if box[0] > size[0]:
             size[0] = box[0]
         if box[1] > size[1]:
@@ -164,28 +164,28 @@ def imjoin(impaths, spath, poses, padding=0, box=None, bg="#ffffff00",
 
 
 def getboxpos(size, imsize, align):
-    '''Calculates position relative to the inner frame of the image's
-    position. Used in the imjoin() function.'''
+    """Calculates position relative to the inner frame of the image's
+    position. Used in the imjoin() function."""
     if size == imsize:
-        return (0, 0)
+        return 0, 0
     elif align == "upper left":
-        return (0, 0)
+        return 0, 0
     elif align == "center left":
-        return (0, (size[1]-imsize[1])//2)
+        return 0, (size[1] - imsize[1]) // 2
     elif align == "lower left":
-        return (0, size[1]-imsize[1])
+        return 0, size[1] - imsize[1]
     elif align == "upper center":
-        return ((size[0]-imsize[0])//2, 0)
+        return (size[0] - imsize[0]) // 2, 0
     elif align == "center center":
-        return ((size[0]-imsize[0])//2, (size[1]-imsize[1])//2)
+        return (size[0] - imsize[0]) // 2, (size[1] - imsize[1]) // 2
     elif align == "lower center":
-        return ((size[0]-imsize[0])//2, size[1]-imsize[1])
+        return (size[0] - imsize[0]) // 2, size[1] - imsize[1]
     elif align == "upper right":
-        return (size[0]-imsize[0], 0)
+        return size[0] - imsize[0], 0
     elif align == "center right":
-        return (size[0]-imsize[0], (size[1]-imsize[1])//2)
+        return size[0] - imsize[0], (size[1] - imsize[1]) // 2
     elif align == "lower right":
-        return (size[0]-imsize[0], size[1]-imsize[1])
+        return size[0] - imsize[0], size[1] - imsize[1]
     else:
         raise Exception("The align argument getboxpos() has wrong format.")
 
@@ -219,7 +219,7 @@ def pic2text(impath, spath=None, iscale=1, nchars=2, chars=None, maxwidth=None,
     nums = ("0", "01", "012", "0123", "01234", "012345", "0123456", "01234567",
             "012345678", "0123456789")
     styles = {"default": default, "hi-contrast": hicont, "numbers": nums}
-    if chars == None:
+    if chars is None:
         maps = styles[style]
     else:
         maps, nchars = [chars]*10, len(chars)
@@ -233,7 +233,7 @@ def pic2text(impath, spath=None, iscale=1, nchars=2, chars=None, maxwidth=None,
         w, h = size[0]//iscale[0], size[1]//iscale[1]
     else:
         raise Exception("The iscale parameter has unsupported format!")
-    im = im.resize((w,h)).quantize(nchars).convert("L")
+    im = im.resize((w, h)).quantize(nchars).convert("L")
     # print(im.getcolors())
     mappin, colors = dict(), im.getcolors()
     if len(colors) < nchars:
@@ -245,17 +245,16 @@ def pic2text(impath, spath=None, iscale=1, nchars=2, chars=None, maxwidth=None,
         for i in range(nchars):
             mappin[colors[i][1]] = nchars-1-i
     # im.show()
-    if spath == None:
+    if spath is None:
         for j in range(h):
             st = ""
             for i in range(w):
-                st += maps[nchars][mappin[im.getpixel((i,j))]]
+                st += maps[nchars][mappin[im.getpixel((i, j))]]
             print(st)
     else:
         with open(spath, "w+") as file:
             for j in range(h):
                 for i in range(w):
-                    file.write(maps[nchars][mappin[im.getpixel((i,j))]])
+                    file.write(maps[nchars][mappin[im.getpixel((i, j))]])
                 file.write("\n")
     im.close()
-
